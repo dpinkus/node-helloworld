2f3a5341d035584855b0e5c73f1a7a043adb94a7
dpinkus <david@pinkus.com>
2015-10-28 09:56:36 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+var name = "David";
+var details = { favoriteLanguage: "JavaScript", age: 46, children: 4 }
+var errorMessage = "Something bad has occurred";
+console.log(name);
+console.dir(details);
+console.error(errorMessage);
