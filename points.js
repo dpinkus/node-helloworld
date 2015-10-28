d0940a1ab7bdfd0ccad90a9b17da7a4768b39000
dpinkus <david@pinkus.com>
2015-10-28 11:57:06 -0700

    Problem statement for points console app

---
+++ points.js
@@ -0,0 +1,8 @@
+// Problem:  Get the points that a student has at their Treehouse account.
+// It will come from teamtreehouse.com/username.json
+// Solutions;  Use Node.js to connect to Treehouse API to get profile and points.
+
+// Connect ot API URL (http://teamtreehouse.com/username.json)
+// Read the data
+// Parse the data
+// Print the data
